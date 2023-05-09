5fa9513398177a88ee7eeaf4b9ef3f7d0893d62b
Pavel Vitov <ivov.vitov@gmail.com>
2023-05-09 12:31:35 +0300

    Services create, IServices create.

---
+++ BusinessLayer/Services/UserService.cs
@@ -0,0 +1,14 @@
+﻿using BusinessLayer.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public class UserService : IUserService
+    {
+
+    }
+}
